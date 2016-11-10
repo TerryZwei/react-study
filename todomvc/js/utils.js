f87623996b1c0319f56d0b6a574cb3821c3cbd95
Terry <gyweizhiwei@gmail.com>
2016-11-10 11:51:43 +0800

    fixbug

--- todomvc/js/utils.js
+++ todomvc/js/utils.js
@@ -1,54 +1,61 @@
 var app = app || {};
 
 (function () {
-  'use strict';
+	'use strict';
 
-  app.Utils = {
+	app.Utils = {
     /**
-     * 生成一个随机的uuid
+     * 生成一个随机的id
      */
-    uuid: function () {
-      // 生成一个随机的
-      var i, random;
-      var uuid = '';
+		uuid: function () {
+			/*jshint bitwise:false */
+			var i, random;
+			var uuid = '';
 
-      for (i = 0; i < 32; i++) {
-        // 生成一个32位的uuid
-        random = Math.random() * 16 | 0;
-        if (i === 8 || i === 12 || i === 16 || i === 20) {
-          uuid += '-';
-        }
-        uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8): random)).toString(16);
-      }
-    },
+			for (i = 0; i < 32; i++) {
+				random = Math.random() * 16 | 0;
+				if (i === 8 || i === 12 || i === 16 || i === 20) {
+					uuid += '-';
+				}
+				uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random))
+					.toString(16);
+			}
 
-    pluralize: function (count, word) {
-      return count === 1 ? word : word + 's';
-    },
+			return uuid;
+		},
+    /**
+     *
+     */
+		pluralize: function (count, word) {
+			return count === 1 ? word : word + 's';
+		},
 
     /**
-     * 存储、获取数据
+     * 存储、获取localstorage的值
      */
-    store: function (namespace, data) {
-      // set data to localStorage
-      if (data) {
-        return localStorage.setItem(namespace, JSON.stringify(data));
-      }
-      // get data from localStorage
-      var store = localStorage.getItem(namespace);
-      return (store && JSON.parse(store)) || [];
-    },
+		store: function (namespace, data) {
+			if (data) {
+				return localStorage.setItem(namespace, JSON.stringify(data));
+			}
 
-    extend: function () {
-      var newObj = {};
-      for (var i = 0; i < arguments.length; i++) {
-        var obj = arguments[i];
-        for (var key in obj) {
-          if (obj.hasOwnProperty(key)) {
-            newObj[key] = obj[key];
-          }
-        }
-      }
-    }
-  };
-} ());
+			var store = localStorage.getItem(namespace);
+			return (store && JSON.parse(store)) || [];
+		},
+
+    /**
+     * 扩展属性值
+     */
+		extend: function () {
+			var newObj = {};
+			for (var i = 0; i < arguments.length; i++) {
+				var obj = arguments[i];
+				for (var key in obj) {
+					if (obj.hasOwnProperty(key)) {
+						newObj[key] = obj[key];
+					}
+				}
+			}
+			return newObj;
+		}
+	};
+})();
